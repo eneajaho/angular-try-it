d2e4083dd2cfa1a584a93bd20e786ce2ffd7ca3f
eneajaho <jahollarienea14@gmail.com>
2020-12-09 19:32:49 +0100

    Removed rxjs stuff

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,20 +1,17 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { BehaviorSubject, Observable } from "rxjs";
+import { Observable } from "rxjs";
 import { Product } from "./models/Product";
 import { ShippingType } from "./models/ShippingType";
 
 @Injectable({ providedIn: "root" })
 export class CartService {
-  readonly count$ = new BehaviorSubject(0);
-
   private items: Product[] = [];
 
   constructor(private http: HttpClient) {}
 
   addToCart(product): void {
     this.items.push(product);
-    this.count$.next(this.items.length);
   }
 
   getItems(): Product[] {
@@ -23,7 +20,6 @@ export class CartService {
 
   clearCart(): Product[] {
     this.items = [];
-    this.count$.next(this.items.length);
     return this.items;
   }
 
--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,17 +1,11 @@
 import { Component } from "@angular/core";
-import { Observable } from "rxjs";
-import { CartService } from "../cart.service";
 
 @Component({
   selector: "app-top-bar",
   templateUrl: "./top-bar.component.html",
   styleUrls: ["./top-bar.component.css"]
 })
-export class TopBarComponent {
-  cartCount$: Observable<number> = this.cartService.count$;
-
-  constructor(private cartService: CartService) {}
-}
+export class TopBarComponent {}
 
 /*
 Copyright Google LLC. All Rights Reserved.
