58a0ecb3562a79c33e32dbbbc44bc9587ce0228c
eneajaho <jahollarienea14@gmail.com>
2020-12-29 18:01:05 +0100

    2 files modified

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,11 +1,17 @@
 import { Component } from "@angular/core";
+import { CartService } from "../cart.service";
 
 @Component({
   selector: "app-top-bar",
   templateUrl: "./top-bar.component.html",
   styleUrls: ["./top-bar.component.css"]
 })
-export class TopBarComponent {}
+export class TopBarComponent {
+  // this won't change
+  count = this.cart.getItems().length;
+
+  constructor(private cart: CartService) {}
+}
 
 /*
 Copyright Google LLC. All Rights Reserved.
